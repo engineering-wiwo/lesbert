a72bd8bb2e9150ec7c679104e5c350ea9f739c04
b4nn3d0124 <b4nn3d.0124@gmail.com>
2026-04-07 07:41:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
+
 const CONFIG = {
   API_URL: "https://asset-api-proxy.b4nn3d-0124.workers.dev/",
   ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbzwAbpJb1KZnP8Y4jl9YUkM996TJP2BTs7ImAbo0oYWsFtMsgmEkiiqo8c0gEWCCIA/exec",
   JSON_API_URL: "https://script.google.com/macros/s/AKfycbwvkADinvGO_ZnTzmVM4owXMf2uBN_Hba7LXgHu-AJFNHVaVqRiW_ENY-24JV9AA_AJ/exec",
   GOOGLE_CLIENT_ID: "955024166373-hjrj6splihhs19pj96fgpv0ri4cf7u2q.apps.googleusercontent.com",
-  COMPANY_DOMAIN: "gmail.com",
-  SUPERADMIN_PASSWORD: "BorrowSmart@2026"
+  COMPANY_DOMAIN: "gmail.com"
 };
 
 window.CONFIG = CONFIG;
