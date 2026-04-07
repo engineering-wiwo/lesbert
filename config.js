e60b9d9e85671853a77426602fb587af122d167d
b4nn3d0124 <b4nn3d.0124@gmail.com>
2026-04-07 15:28:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,38 +8,3 @@ const CONFIG = {
 };
 
 window.CONFIG = CONFIG;
-
-// Add a debug log to verify the config is loaded correctly
-console.log('CONFIG loaded:', CONFIG);
-console.log('ADMIN_API_URL:', CONFIG.ADMIN_API_URL); 
-
-window.apiRequest = async function(params = {}, method = "POST") {
-  const url = CONFIG.ADMIN_API_URL;
-
-  try {
-    if (method === "GET") {
-      const query = new URLSearchParams(params).toString();
-      const res = await fetch(url + "?" + query, {
-        method: "GET",
-        headers: { "Accept": "application/json" }
-      });
-      return await res.json();
-    }
-
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(params)
-    });
-
-    return await res.json();
-  } catch (err) {
-    console.error("API request failed:", err);
-    return {
-      success: false,
-      error: err.message
-    };
-  }
-};
