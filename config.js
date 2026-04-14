b01b8a870e30940144eca936a5b48b88ad5c12bb
Banned <b4nn3d.0124@gmail.com>
2026-04-14 14:12:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 
 const CONFIG = {
-  API_URL: "https://asset-api-proxy.b4nn3d-0124.workers.dev/",
-  ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbzwAbpJb1KZnP8Y4jl9YUkM996TJP2BTs7ImAbo0oYWsFtMsgmEkiiqo8c0gEWCCIA/exec",
-  JSON_API_URL: "https://script.google.com/macros/s/AKfycbwvkADinvGO_ZnTzmVM4owXMf2uBN_Hba7LXgHu-AJFNHVaVqRiW_ENY-24JV9AA_AJ/exec",
-  GOOGLE_CLIENT_ID: "955024166373-hjrj6splihhs19pj96fgpv0ri4cf7u2q.apps.googleusercontent.com",
+  API_URL: "https://asset-api-proxy.b4nn3d-0124.workers.dev/", //For User/Assets (pero naka cloudFare)
+  ADMIN_API_URL: "https://script.google.com/macros/s/AKfycbzwAbpJb1KZnP8Y4jl9YUkM996TJP2BTs7ImAbo0oYWsFtMsgmEkiiqo8c0gEWCCIA/exec", //For Admin Sheets
+  //JSON_API_URL: "https://script.google.com/macros/s/AKfycbwvkADinvGO_ZnTzmVM4owXMf2uBN_Hba7LXgHu-AJFNHVaVqRiW_ENY-24JV9AA_AJ/exec",
+  //GOOGLE_CLIENT_ID: "955024166373-hjrj6splihhs19pj96fgpv0ri4cf7u2q.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "710899589922-ham38q1s57pt83ptjbip543imdohl2r6.apps.googleusercontent.com",// Cloud ID Client
   COMPANY_DOMAIN: "wilsonworksph.com"
 };
 
